31566c6ab30059c4581d9e27ff0e2f882be7d524
ratigumashvili <ratigumashvili@gmail.com>
2024-08-25 23:39:34 +0400

    cleanup

--- src/app/[locale]/components/Footer.jsx
+++ src/app/[locale]/components/Footer.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 import { useTranslations } from "next-intl"
 
 function Footer() {
@@ -6,7 +8,7 @@ function Footer() {
         <div className="bg-slate-900 text-white text-xs font-light py-6 px-5 mt-auto">
             <div className="max-w-7xl mx-auto text-center">
                 <p className="mb-2">&copy; 2024 {t("isu")}</p>
-                <p>{t("mail")}</p>
+                <Link href={`mailto:${t('mail')}`}>{t("mail")}</Link>
             </div>
         </div>
     )
--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -1,16 +1,18 @@
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 
-import { firaGo } from '../lib/fonts';
-
-import "./globals.css";
-
 import Header from './components/Header';
 import Footer from './components/Footer';
+
+import { firaGo } from '../lib/fonts';
+import "./globals.css";
 
 export const metadata = {
   title: "Legacy of Catholic Missions",
   description: "Religious fragmentation and connectedness in Georgia in the 18th and 19th centuries",
+  openGraph: {
+    images: 'https://res.cloudinary.com/djvaj2g97/image/upload/v1724447311/banner_b45cc4ea8b.png'
+  }
 };
 
 export default async function LocaleLayout({ children, params: { locale } }) {
@@ -21,6 +23,7 @@ export default async function LocaleLayout({ children, params: { locale } }) {
     <html lang={locale}>
       <body className={`${firaGo.variable} font-firaGo font-light bg-slate-50`}>
         <NextIntlClientProvider messages={messages}>
+          
           <div className='flex flex-col min-h-screen'>
             <Header locale={locale} />
             <main className='max-w-7xl w-full mx-auto py-8 px-5'>
@@ -28,6 +31,7 @@ export default async function LocaleLayout({ children, params: { locale } }) {
             </main>
             <Footer />
           </div>
+
         </NextIntlClientProvider>
       </body>
     </html>
