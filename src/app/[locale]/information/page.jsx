eb564865e98521f2d912d11195509c57da11aef0
ratigumashvili <ratigumashvili@gmail.com>
2024-08-26 22:25:44 +0400

    cleanup

--- src/app/[locale]/information/page.jsx
+++ src/app/[locale]/information/page.jsx
@@ -14,7 +14,7 @@ async function PracticalInformation({ params: { locale } }) {
       <MDContent content={response?.data?.attributes?.content} />
 
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d1489.1818609627526!2d44.74808929832448!3d41.712669638585254!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sIlia%20State%20University%20building%20F!5e0!3m2!1sen!2sge!4v1724662901919!5m2!1sen!2sge"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2978.5341948649416!2d44.76830807535206!3d41.70899037598625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40447332cec3b2d9%3A0xcf4f8584f7256685!2sLigamus!5e0!3m2!1sen!2sge!4v1724695526124!5m2!1sen!2sge"
         width="100%"
         height="450"
         style={{border: "0"}}
